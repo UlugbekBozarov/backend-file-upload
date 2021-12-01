2d078aff3b1463006ee741531a35f049ec6c4540
UlugbekBozarov <ulugbek01081999@gmail.com>
2021-12-01 16:16:08 +0500

    finished project

--- src/main/java/example/demo/DemoApplication.java
+++ src/main/java/example/demo/DemoApplication.java
@@ -1,14 +1,26 @@
 package example.demo;
 
+import example.demo.service.FilesStorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+    @Resource
+    FilesStorageService storageService;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("Hello world!");
+    }
+
+    @Override
+    public void run(String... arg) throws Exception {
+        storageService.deleteAll();
+        storageService.init();
     }
 
 }
